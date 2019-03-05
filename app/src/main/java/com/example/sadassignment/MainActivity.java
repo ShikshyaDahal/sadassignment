e6bddf0b8f52e660745dfcfb9fdf211c127fa421
bibhutiregmi1 <bibhutiregmi1@gmail.com>
2019-03-05 22:31:37 +0700

    adding signup java page

--- app/src/main/java/com/example/sadassignment/MainActivity.java
+++ app/src/main/java/com/example/sadassignment/MainActivity.java
@@ -1,15 +1,47 @@
 package com.example.sadassignment;
 
-import android.support.annotation.Nullable;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity{
-
+public class MainActivity extends AppCompatActivity {
+    Button signup;
+    TextView login;
+    EditText email, password, address, phone, name;
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        signup = findViewById(R.id.signup);
+        login = findViewById(R.id.login);
+
+        name = findViewById(R.id.name);
+        email = findViewById(R.id.email);
+        password = findViewById(R.id.password);
+        address = findViewById(R.id.address);
+        phone = findViewById(R.id.phone);
+
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Signup", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
 
     }
 }
